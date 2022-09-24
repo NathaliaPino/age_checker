7ff6393cb5ad5671f5583a988110fc91439e3b6c
NathaliaPino <101751308+NathaliaPino@users.noreply.github.com>
2022-09-23 23:14:16 -0300

    body

---
+++ script.js
@@ -0,0 +1,15 @@
+function verificar() {
+  var texto = window.document.querySelector("#texto");
+  var resposta = window.document.querySelector(".resposta");
+  var dataHoje = new Date();
+  var pegarAno = dataHoje.getFullYear();
+
+  if (texto.value.length == 0 || Number(texto.value) > pegarAno) {
+    window.alert("ERRO! Verifique os dados e tente novamente");
+  } else {
+    var calculoIdade = pegarAno - Number(texto.value);
+    var radios = window.document.getElementsByName("radio");
+    var genero = radios[0].checked ? "mulher" : "homem";
+    resposta.innerHTML = `Detectamos ${genero} com ${calculoIdade} anos (ou que vai fazer ${calculoIdade} anos, este ano) `;
+  }
+}
